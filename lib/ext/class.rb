7b57c1ca00030219c0fd7f5b8cbeaaddc74a3796
Stephen Touset <stephen@touset.org>
2010-02-05 09:07:05 -0500

    Allow Class#is_versioned to actually work

--- lib/ext/class.rb
+++ lib/ext/class.rb
@@ -3,13 +3,14 @@ require 'pathname'
 class Class
   #
   # Automagically sets a VERSION constant in the current class, populated by
-  # the Version in +filename+. Attempts to guess the filename if none given.
-  # Can read both .yml files and plain text.
+  # the Version in +filename+. Attempts to guess the filename of the VERSION
+  # file by looking for VERSION or VERSION.yml files in the directories above
+  # the caller
   #
   def is_versioned(filename = nil)
     # attempt to guess the filename if none given
     if filename.nil?
-      filename = Pathname.new('.').expand_path.ascend do |d|
+      filename = Pathname.new(caller.first).dirname.expand_path.ascend do |d|        
         break d.join('VERSION')     if d.join('VERSION').exist?
         break d.join('VERSION.yml') if d.join('VERSION.yml').exist?
       end
