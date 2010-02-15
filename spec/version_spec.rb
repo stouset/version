c366a5e88ea62df52acf90c688a8cb8176b2d508
Stephen Touset <stephen@touset.org>
2010-02-15 15:33:26 -0500

    Better tests, and they pass now!

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,6 +1,56 @@
 require 'spec_helper'
 
+module ImplicitVersion
+  def method_missing(name, *args, &block)
+    super unless args.empty?
+    super unless block.nil?
+    super unless name.to_s =~ /^v[\d\w_]+$/
+    
+    name.to_s.gsub(/^v/, '').gsub(/_/, '.').to_version
+  end
+end
+
 describe Version do
+  include ImplicitVersion
+  
+  subject { "1.6.3a".to_version }
+  
+  its(:major)       { should == '1'  }
+  its(:minor)       { should == '6'  }
+  its(:revision)    { should == '3a' }
+  its(:prerelease?) { should be_true }
+  
+  it 'should allow indexed access to components' do
+    subject[0].should == '1'
+    subject[1].should == '6'
+    subject[2].should == '3a'
+  end
+  
+  it 'should return nil for unset components' do
+    subject[3].should == nil
+    subject[4].should == nil
+  end
+  
+  it 'should major-bump to 2.0.0' do
+    subject.bump!(:major).should == v2_0_0
+  end
+  
+  it 'should minor-bump to 1.7.0' do
+    subject.bump!(:minor).should == v1_7_0
+  end
+  
+  it 'should revision-bump to 1.6.3' do
+    subject.bump!(:revision).should == v1_6_3
+  end
+  
+  it 'should prerelease-bump to 1.6.3b' do
+    subject.bump!(:pre).should == v1_6_3b
+  end
+end
+
+describe Version do
+  include ImplicitVersion
+  
   it 'should preserve equality' do
     v0_0.should       == v0_0
     v0_1_1.should     == v0_1_1
@@ -19,13 +69,5 @@ describe Version do
     v0_1a.should    < v0_1
     v0_01.should    < v0_10
     v0_9.should     < v0_10
-  end
-  
-  def method_missing(name, *args, &block)
-    super unless args.empty?
-    super unless block.nil?
-    super unless name.to_s =~ /^v[\d\w_]+$/
-    
-    name.to_s.gsub(/^v/, '').gsub(/_/, '.').to_version
   end
 end
